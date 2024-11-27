0744cf47a0e44a2883d462b72858f9749bfe5e5d
Haley <hs.haleyk@gmail.com>
2024-11-26 23:00:44 -0500

    revisioned app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import EmployeeList from "./components/employees";
+import AddEmployee from "./components/addEmployee";
+import ViewEmployee from "./components/viewEmployee";
+import EditEmployee from "./components/editEmployee";
+import PrivateRoute from "./components/privateRoute";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // React.useEffect(() => {
+    //     localStorage.removeItem("token");
+    // }, []);
+
+    const isAuthenticated = !!localStorage.getItem("token");
+
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Navigate to={isAuthenticated ? "/employees" : "/login"} replace />}
+                />
+
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+
+                <Route
+                    path="/employees"
+                    element={
+                        <PrivateRoute>
+                            <EmployeeList />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/employees/add"
+                    element={
+                        <PrivateRoute>
+                            <AddEmployee />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/employees/:id"
+                    element={
+                        <PrivateRoute>
+                            <ViewEmployee />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/employees/edit/:id"
+                    element={
+                        <PrivateRoute>
+                            <EditEmployee />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
