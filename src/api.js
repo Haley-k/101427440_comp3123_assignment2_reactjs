3ce4f839e92a7354271b45ac21a46f3b4fc121ee
Haley <hs.haleyk@gmail.com>
2024-11-27 11:28:07 -0500

    edit baseURL for backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://comp3123-assignment2.vercel.app/",
 });
 
 api.interceptors.request.use(
