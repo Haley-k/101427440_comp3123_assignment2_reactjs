3c4d229d8541291a564b86fb135799f39c0ccd5b
Haley <hs.haleyk@gmail.com>
2024-11-27 11:54:44 -0500

    edit api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://comp3123-assignment2.vercel.app/",
+    baseURL: "http://localhost:8000",
 });
 
 api.interceptors.request.use(
